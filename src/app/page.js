5f27a928efcfa30bd6c0d44a0bdb60c92a4b7ccb
sushantn16 <nandwani160698@gmail.com>
2023-12-12 15:04:01 +0100

    view all button added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Carousel from "@/layout/carousel";
 import { useDataContext } from "@/context/data";
 import Card from "@/layout/card";
+import Link from "next/link";
 
 export default function Home() {
   const [data, setData] = useDataContext()
@@ -11,11 +12,14 @@ export default function Home() {
         <Carousel />
       </div>
       <div className="mt-10">
-        <p className="text-2xl text-orange mx-5">
-          Trending Blogs
-        </p>
+        <div className="flex justify-between">
+          <p className="text-2xl text-orange mx-5">
+            Trending Blogs
+          </p>
+          <Link className="text-xl text-orange mx-5 hover:underline" href="/blogs">View All</Link>
+        </div>
         <div className="page-content flex flex-wrap">
-          {data.slice(0,3).map((blog) => (
+          {data.slice(0, 3).map((blog) => (
             <Card key={blog.id} href={blog.href} title={blog.title} content={blog.content} image={blog.image} alt={blog.alt} />
           ))}
         </div>
